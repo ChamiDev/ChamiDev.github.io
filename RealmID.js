99b1c22c729f5266ec558aee9a5d5ab3ee5d77df
Chami <pc.ch4m1@gmail.com>
2025-05-16 18:01:50 -0300

    Update RealmID.js

--- RealmID.js
+++ RealmID.js
@@ -1,2 +1,5 @@
-const MAGIC_CRAFT = "6YLPC5etBLUggA8";
-const ONE_BLOCK = "EjemploIDNuevo123";
+const MAGIC_CRAFT_ID = "6YLPC5etBLUggA8";
+const MAGIC_CRAFT_PNG = "MC.png";
+
+const ONE_BLOCK_ID = "EjemploIDNuevo123";
+const ONE_BLOCK_PNG = "OB.png";
